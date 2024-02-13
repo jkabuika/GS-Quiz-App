3819f488f9a03ea3876d595d7668bb11e2ea6b43
jkabuika <jkabuika1319@gmail.com>
2024-02-12 23:14:38 -0500

    Added JS naming booleans

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+      question: "What is the largest animal in the world?"
+      answers: [
+        { text: "Shark", correct: false},
+        { text: "Blue Whale", correct: true},
+        { text: "Elephant", correct: false},
+        { text: "Polar Bear", correct: false},
+      ]
+  },
+  {
+    question: "What is the smallest continent in the world?"
+    answers: [
+      { text: "Asua", correct: false},
+      { text: "Artic", correct: false},
+      { text: "Africa", correct: false},
+      { text: "Australia", correct: true},
+    ]
+  }
+]
