9026f6db942a8770a3fd8bbde0ed81389f516251
jkabuika <jkabuika1319@gmail.com>
2024-02-12 23:16:12 -0500

    10 mins of coding done

--- script.js
+++ script.js
@@ -11,10 +11,19 @@ const questions = [
   {
     question: "What is the smallest continent in the world?"
     answers: [
-      { text: "Asua", correct: false},
+      { text: "Asia", correct: false},
       { text: "Artic", correct: false},
       { text: "Africa", correct: false},
       { text: "Australia", correct: true},
+    ]
+  }
+  {
+    question: "What is the largest desert in the world?"
+    answers: [
+      { text: "Antartica", correct: true},
+      { text: "Sahara", correct: false},
+      { text: "Kalahari", correct: false},
+      { text: "Gobi", correct: false},
     ]
   }
 ]
