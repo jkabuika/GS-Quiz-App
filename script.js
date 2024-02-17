161903b8d9718dd8116c9b565bdde73109628eea
jkabuika <jkabuika1319@gmail.com>
2024-02-16 22:10:55 -0500

    Added function to make the quiz start at 0 score and 0 questions answered

--- script.js
+++ script.js
@@ -32,3 +32,12 @@ const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
