20974c025143a097ba17fee5d523d0c3fa544fd6
jkabuika <jkabuika1319@gmail.com>
2024-02-17 20:11:55 -0500

    New function for showQuestion which will change the questions one after another

--- script.js
+++ script.js
@@ -41,3 +41,9 @@ function startQuiz(){
   nextButton.innerHTML = "Next";
   showQuestion();
 }
+
+function showQuestion(){
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
