4b8548b41d08028f2d8efc110b83cd2915404ec9
jkabuika <jkabuika1319@gmail.com>
2024-02-18 22:22:35 -0500

    JS for nextBtn at tie end

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ function showScore(){
   resetState();
   questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
   nextButton.innerHTML = "Play Again"
+  nextButton.style.display = "block";
 }
 
 function handleNextButton(){
